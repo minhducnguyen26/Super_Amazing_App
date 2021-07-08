1682146143d5a9427b67d2709622c9b267c5219f
froggyfish <masterkl2003@gmail.com>
2021-07-08 13:00:30 -0600

    added app.js post

--- public/app.js
+++ public/app.js
@@ -1,4 +1,48 @@
 var app = new Vue({
     el  : "#app",
-    data:  {}
+    data:  {
+        author: "",
+        newText: "",
+        texts: [],
+        server_url: "http://localhost:8080",
+
+    },
+    methods:{
+
+
+
+
+
+
+
+
+
+        postText: function(){
+            //if(this.author != null && this.author != "" && this.newText != null && this.newText != ""){
+                myData = {
+                    author: this.author,
+                    text: this.newText
+                }
+                fetch(this.server_url+"/text", {
+                    method: "POST",
+                    body: JSON.stringify(myData),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }).then(function(response){
+    
+                })
+
+                this.texts.push(myData)
+            //}
+        }
+    }
 })
+
+
+
+
+
+
+
+
