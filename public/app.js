86bdf395a15025556d4ddd76720cbe2e3a07e239
jandirdlctm <d00332071@dmail.dixie.edu>
2021-07-08 12:58:57 -0600

    fetching data from server done

--- public/app.js
+++ public/app.js
@@ -1,4 +1,18 @@
 var app = new Vue({
     el  : "#app",
-    data:  {}
+    data:  {
+        url: "http://localhost:8080",
+        texts: []
+    },
+    methods:{
+        getPostsFromServer: function(){
+            fetch(`${this.url}/text`).then(function(response){
+                console.log("This is the response", response);
+                response.json().then(function(data){
+                    console.log("This is the data: ", data);
+                    app.texts = data;
+                })
+            })
+        }
+    }
 })
